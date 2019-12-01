dca5f4cc0bdd77ada3f905981cba4a9751f13c16
jeffchoi72 <ihello0720@gmail.com>
2019-12-02 00:45:19 +0900

    refactor: posts 데이터에 content를 추가한다

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -4,23 +4,28 @@ import { Service } from 'typedi';
 const posts = [
   {
     id: "5",
-    title: "5번째 게시글 입니다"
+    title: "5번째 게시글 입니다",
+    content: "5번째 글 내용"
   },
   {
     id: "4",
-    title: "4번째 게시글 입니다"
+    title: "4번째 게시글 입니다",
+    content: "4번째 글 내용"
   },
   {
     id: "3",
-    title: "3번째 게시글 입니다"
+    title: "3번째 게시글 입니다",
+    content: "3번째 글 내용"
   },
   {
     id: "2",
-    title: "2번째 게시글 입니다"
+    title: "2번째 게시글 입니다",
+    content: "2번째 글 내용"
   },
   {
     id: "1",
-    title: "1번째 게시글 입니다"
+    title: "1번째 게시글 입니다",
+    content: "1번째 글 내용"
   }
 ];
 
